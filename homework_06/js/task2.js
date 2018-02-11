8d96107f9a1e86ab4cc8de0d4cb83dde83fbb05a
Andrii Loza <loza.andriy7@gmail.com>
2018-02-11 19:00:11 +0200

    minimization and simplification of code

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,30 +1,17 @@
 window.onload = function(){
 
-    var newA,newB,eu_dol;
     var a = +prompt('Type euro: ');
     var b = +prompt('Type dollar: ');
+    a = parseFloat(a);
+    b= parseFloat(b);
 
-    function converter(one,two){
-        let eu_hr = 33.85;
-        let dol_hr = 27.46;
+    let eu_hr = 33.85;
+    let dol_hr = 27.46;
 
-        if(one>=0 && two >=0){
-            newB = two * dol_hr;
-            newA = one * eu_hr;
-            eu_dol = eu_hr/dol_hr;
-        }
-        return newA,newB,eu_dol;
+    if(a>=0 && b >=0 && !isNaN(a) && !isNaN(b) ){
+        console.log(a+' euros are equal '+ Math.floor(a*eu_hr) + ' UAH, '+ b + ' dollars are equal '+ Math.floor(b*dol_hr) + ' UAH, one euro is equal '+ Math.floor((eu_hr/dol_hr)*1000)/1000 +' dollars');
     }
-
-    function output(){
-        converter(a,b);
-        newA = newA.toFixed(2);
-        newB = newB.toFixed(2);
-        eu_dol = eu_dol.toFixed(2);
-
-        console.log(a+' euros are equal '+ newA + ' UAH, '+ b + ' dollars are equal '+ 
-        newB + ' UAH, one euro is equal '+ eu_dol +' dollars');
+    else{
+        console.log('Incorrect data');
     }
-
-    output();
-}
+}   
