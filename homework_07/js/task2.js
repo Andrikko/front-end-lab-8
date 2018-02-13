4fbe3471534209425b5ae0d255d60a5a155184cc
Andrii Loza <loza.andriy7@gmail.com>
2018-02-13 23:08:36 +0200

    latest update

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,13 +1,10 @@
-window.onload = function(){
-
     var start = confirm('Do you want to play a game?');
-    var price = [10,5,2];
-    var i = 0;
-    var win = 0;
+    var prize_max = 10;
+    var price = [Math.floor(prize_max),Math.floor(prize_max/2),Math.floor(prize_max/4)];
+    var i,win,winN,won,max,end;
     var winN = 0;
     var won= 0;
     var max = 5;
-    let end;
 
     if(start){
         game();
@@ -15,57 +12,41 @@ window.onload = function(){
         console.log('You did not become a millionaire');
     }
 
-    function getPrice(guesst){
-        let res = 0;
-        switch(guesst){
-            case 0: 
-                res = price[0];
-                break;
-            case 1: 
-                res = price[1];
-                break;
-            case 2: 
-                res = price[2];
-                break;
-        }
-        return res;
-    }
-
     function game(){
-        i=0; win = 0;
+        i=0; 
+        win = 0;
         while(i!==3 && end !== true){
             let range = Math.floor(Math.random()*(max+1));
             let num = prompt('Enter a number from 0 to '+max+'\nAttempts left: '+i+"\nTotal prize: "+winN+'$\n'+'Possible prize on current attempt: '+price[i]+'$' );
-
+            if(num === null){ break };
             if(range==num){
-                win = getPrice(i);
+                win = price[i];
                 let cont = confirm('You guessed it!\nDo you want to continue a game?');
                 if(cont){
                     max = max * 2;
-                    for(let j=0; j<price.length; j++){
-                        price[j] = price[j] * 3;
-                    }
-                    i=0; winN+= win;
+                    prize_max = prize_max * 3;
+                    price = [Math.floor(prize_max),Math.floor(prize_max/2),Math.floor(prize_max/4)];
+                    winN+= win;
                     game();
                 }else{
                     winN+= win;
                     end = true;
                     won += winN;
                     console.log('Thank you for a game. Your prize is: '+ won);
-                    alert('Thank you for a game. Your prize is: '+ won);
                     break; 
                 }
             }
             
             i++;
             if(i==3){
+                won=0;
                 let again = confirm('Do you want to play again?');
                 if(again){
-                    price = [10,5,2];
+                    prize_max = 10;
+                    price = [Math.floor(prize_max),Math.floor(prize_max/2),Math.floor(prize_max/4)];
                     end = false;
                     game();
-                }
-                else{
+                }else{
                     end = true;
                     won += winN;
                     console.log('Thank you for a game. Your prize is: '+ won);
@@ -73,8 +54,3 @@ window.onload = function(){
             }
         }
     }
-    
-    
-    
-        
-}
