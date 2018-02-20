eed852578ad553166de985f505557827e7ed53db
Andrii Loza <loza.andriy7@gmail.com>
2018-02-20 20:49:22 +0200

    add task 3

---
+++ homework_09/js/getFilteredArray.js
@@ -0,0 +1,10 @@
+function getFilteredArray(arr, func) {
+    var newArr = new Array();
+    function sort(element) {
+      if (func(element)) {
+        newArr.push(element);
+      }
+    }
+    forEach(arr, sort);
+    return newArr;
+  }
