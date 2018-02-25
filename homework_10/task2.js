4232093450a1b4189475f6918d599a8e16c1e227
Andrii Loza <loza.andriy7@gmail.com>
2018-02-25 17:08:03 +0200

    added fight function

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -27,77 +27,106 @@
  */
 
 function showResult(fighter) {
-  console.log('Fighter', fighter.getName());
-  console.log('- Combat stats:', fighter.getCombatHistory());
-  console.log('- Properties:', fighter.getStats());
+    console.log('Fighter', fighter.getName());
+    console.log('- Combat stats:', fighter.getCombatHistory());
+    console.log('- Properties:', fighter.getStats());
 }
 
-function fighter( fighter ){
+function fighter(fighter) {
 
-  //let Stats = fighter;
-  //let CombatHistory = { wins: 0, loses: 0};
+    return {
+        CombatHistory: {
+            wins: 0,
+            loses: 0
+        },
+        Stats: {
+            name: fighter.name,
+            attack: fighter.attack,
+            hp: fighter.hp,
+        },
+        getCombatHistory: function () {
+            return {
+                wins: this.CombatHistory.wins,
+                loses: this.CombatHistory.loses
+            }
+        },
+        getName: function () {
+            return this.name;
+        },
+        getStats: function () {
+            return {
+                name: this.Stats.name,
+                attack: this.Stats.attack,
+                hp: this.Stats.hp
+            }
+        },
+        block: function () {
+            return Math.random() < 0.5;
+        },
 
-  return {
-    CombatHistory: {
-      wins: 0,
-      loses: 0
-    },
-    Stats: {
-      name : fighter.name,
-      attack : fighter.attack,
-      hp : fighter.hp,
-    },
-    getCombatHistory: function () {
-      return {
-          wins:  this.CombatHistory.wins,
-          loses:  this.CombatHistory.loses
-      }
-  },
-    getName: function() {
-      return this.name;
-    },
-    getStats: function() {
-      return {
-          name: this.Stats.name,
-          attack:  this.Stats.attack,
-          hp:  this.Stats.hp
-      }
-  },
-    block: function(){
-      return Math.random() < 0.5;
+        fight: function (fighter) {
+            var hit = false;
+
+            if (fighter.Stats.hp > 0 && !fighter.block()) {
+                if (this.Stats.attack > fighter.Stats.hp) {
+                    fighter.Stats.hp = 0;
+                    hit = true;
+                } else {
+                    fighter.Stats.hp -= this.Stats.attack;
+                    hit = true;
+                }
+
+                if (hit && fighter.Stats.hp <= 0) {
+                    this.CombatHistory.wins++;
+                    fighter.CombatHistory.loses++;
+                }
+            }
+
+            return hit;
+        }
     }
-
-  }
 }
 
 
 /**
  * The following code must be valid after implementation!
- */ 
+ */
 
- var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
- //var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
- //var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100}); 
+var fighter1 = fighter({
+    name: 'John',
+    attack: 100,
+    hp: 100
+});
+var fighter2 = fighter({
+    name: 'Kayn',
+    attack: 50,
+    hp: 300
+});
+var fighter3 = fighter({
+    name: 'Bill',
+    attack: 40,
+    hp: 100
+});
 
- //fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
- //fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
+fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
+fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
 // /**
 //  * Fighter John
 //  * - Combat stats: { wins: 1, loses: 0 }
 //  * - Properties: { name: 'John', attack: 100, hp: 100 }
 //  */
- showResult(fighter1);
+showResult(fighter1);
 
 // /** Fighter Kayn
 //  * - Combat stats: { wins: 0, loses: 0 }
 //  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
 //  */
- //showResult(fighter2); 
+showResult(fighter2);
 
 // /**
 //  * Fighter Bill
 //  * - Combat stats: { wins: 0, loses: 1 }
 //  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
 //  */
- //showResult(fighter3);
+showResult(fighter3);
